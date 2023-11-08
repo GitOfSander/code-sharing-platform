89f0756baaaf60f4c80c1d042e728ee977ce2877
SanderDev <sanderpals@live.nl>
2023-11-08 20:34:04 +0100

    Stage 2/5: POST updates
    Created post method and Storing now the code temporarily in a txt file.

--- src/java/platform/controllers/IndexController.java
+++ src/java/platform/controllers/IndexController.java
@@ -1,13 +1,57 @@
 package platform.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Controller
 public class IndexController {
+    private final Path FILE = Paths.get("code.txt");
+
+    private String readFile() {
+        try {
+            if (!Files.exists(FILE)) {
+                Files.createFile(FILE);
+            }
+
+            return Files.readString(FILE);
+        } catch (IOException e) {
+            System.out.println("Something went wrong!");
+        }
+
+        return "";
+    }
+
     @GetMapping("/code")
-    public String getIndex() {
-        return "index";
+    public ResponseEntity<String> getIndex() {
+        String content =
+                "<!DOCTYPE html>\n" +
+                        "\n" +
+                        "<html lang=\"en\">\n" +
+                        "    <head>\n" +
+                        "        <meta charset=\"UTF-8\">\n" +
+                        "        <title>Code</title>\n" +
+                        "        <link rel=\"stylesheet\" href=\"/css/stylesheet.css\" />\n" +
+                        "    </head>\n" +
+                        "    <body>\n" +
+                        "        <span id=\"load_date\">2020/01/01 12:00:03</span>\n" +
+                        "        <pre id=\"code_snippet\">" + readFile() +  "</pre>\n" +
+                        "\n" +
+                        "        <script src=\"/js/get.js\"></script>\n" +
+                        "    </body>\n" +
+                        "</html>";
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.TEXT_HTML);
+
+        return new ResponseEntity<String>(content, responseHeaders, HttpStatus.OK);
     }
 
     @GetMapping("/code/new")
--- src/java/platform/controllers/rest/CodeRestController.java
+++ src/java/platform/controllers/rest/CodeRestController.java
@@ -3,22 +3,69 @@ package platform.controllers.rest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @RestController
 public class CodeRestController {
+    private final Path FILE = Paths.get("code.txt");
+
+    private String readFile() {
+        try {
+            if (!Files.exists(FILE)) {
+                Files.createFile(FILE);
+            }
+
+            return Files.readString(FILE);
+        } catch (IOException e) {
+            System.out.println("Something went wrong!");
+        }
+
+        return "";
+    }
+
+    public void updateFile(String string) {
+        try {
+            Files.writeString(FILE, string);
+        } catch (IOException e) {
+            System.out.println("Something went wrong!");
+        }
+    }
+
     @GetMapping("/api/code")
-    public ResponseEntity<Map<String, String>> getCodeInJson() {
+    public ResponseEntity<Map<String, String>> getCode() {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Content-Type", "application/json");
 
         Map<String, String> map = new HashMap<>();
-        map.put("code", "public static void main(String[] args) {\n" +
-                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
-                "}");
+        map.put("code", readFile());
+
+        //LocalDateTime myDateObj = LocalDateTime.now();
+        //DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy/dd/MM HH:mm:ss");
+        //String formattedDate = myDateObj.format(myFormatObj);
+        map.put("date", "2020/01/01 12:00:03");
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(map);
+    }
+
+    @PostMapping(value = "/api/code/new", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Map<String, String>> postCode(@RequestBody Map<String, String> body) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "application/json");
+
+        updateFile(body.get("code"));
+        Map<String, String> map = new HashMap<>();
 
         return ResponseEntity.ok()
                 .headers(responseHeaders)
